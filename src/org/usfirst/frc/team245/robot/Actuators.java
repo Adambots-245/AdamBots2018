65b4b846393ffcd7c890427f49036fe3882f63e5
Scott Guest <Scott.Guest@live.com>
2018-01-31 20:24:14 -0500

    Corrected joystick and motor inversion

--- src/org/usfirst/frc/team245/robot/Actuators.java
+++ src/org/usfirst/frc/team245/robot/Actuators.java
@@ -4,17 +4,27 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class Actuators {
 
+	// motor controllers
 	private static WPI_TalonSRX rightFrontMotor;
 	private static WPI_TalonSRX rightRearMotor;
 	private static WPI_TalonSRX leftFrontMotor;
 	private static WPI_TalonSRX leftRearMotor;
 
-	//
+	// initialzies all actuators
 	public static void init() {
+
+		// initialize Talons
 		rightFrontMotor = new WPI_TalonSRX(Constants.RIGHT_FRONT_DRIVE_MOTOR_PORT);
 		rightRearMotor = new WPI_TalonSRX(Constants.RIGHT_REAR_DRIVE_MOTOR_PORT);
 		leftFrontMotor = new WPI_TalonSRX(Constants.LEFT_FRONT_DRIVE_MOTOR_PORT);
 		leftRearMotor = new WPI_TalonSRX(Constants.LEFT_REAR_DRIVE_MOTOR_PORT);
+
+		// set Talons inverted
+		rightFrontMotor.setInverted(Constants.RIGHT_FRONT_DRIVE_MOTOR_INVERTED);
+		rightRearMotor.setInverted(Constants.RIGHT_REAR_DRIVE_MOTOR_INVERTED);
+		leftFrontMotor.setInverted(Constants.LEFT_FRONT_DRIVE_MOTOR_INVERTED);
+		leftRearMotor.setInverted(Constants.LEFT_REAR_DRIVE_MOTOR_INVERTED);
+
 	}
 
 	public static WPI_TalonSRX getRightFrontMotor() {
--- src/org/usfirst/frc/team245/robot/Constants.java
+++ src/org/usfirst/frc/team245/robot/Constants.java
@@ -9,6 +9,11 @@ public class Constants {
 	public static final int LEFT_REAR_DRIVE_MOTOR_PORT = 1;
 	public static final int RIGHT_REAR_DRIVE_MOTOR_PORT = 3;
 
+	public static final boolean LEFT_FRONT_DRIVE_MOTOR_INVERTED = true;
+	public static final boolean RIGHT_FRONT_DRIVE_MOTOR_INVERTED = true;
+	public static final boolean LEFT_REAR_DRIVE_MOTOR_INVERTED = true;
+	public static final boolean RIGHT_REAR_DRIVE_MOTOR_INVERTED = true;
+
 	public static final double MIN_MOTOR_SPEED = -1.0;
 	public static final double MAX_MOTOR_SPEED = 1.0;
 
