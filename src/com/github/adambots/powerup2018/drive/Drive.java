9087e542c155d8966da5668891c8a866c3bdcded
Scott Guest <Scott.Guest@live.com>
2018-01-31 20:24:55 -0500

    Added squaring of controller input to allow smoother controlling

--- src/com/github/adambots/powerup2018/drive/Drive.java
+++ src/com/github/adambots/powerup2018/drive/Drive.java
@@ -1,9 +1,6 @@
 package com.github.adambots.powerup2018.drive;
 
 import org.usfirst.frc.team245.robot.Actuators;
-import org.usfirst.frc.team245.robot.Constants;
-
-import com.ctre.phoenix.motorcontrol.ControlMode;
 
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 
@@ -16,29 +13,16 @@ public class Drive {
 				Actuators.getRightFrontMotor(), Actuators.getRightRearMotor());
 	}
 
-	private static double capSpeed(double speed) {
-		speed = Math.min(Constants.MAX_MOTOR_SPEED, speed);
-		speed = Math.max(Constants.MIN_MOTOR_SPEED, speed);
-		return speed;
+	private static double sgnSquare(double x) {
+		return Math.signum(x) * Math.pow(x, 2);
 	}
 
-	public static void mecDrive(double ySpeed, double xSpeed, double zRotation) {
-		robotDrive.driveCartesian(ySpeed, xSpeed, zRotation);
-		// double leftFrontSpeed = ySpeed - zRotation - xSpeed;
-		// double rightFrontSpeed = -(ySpeed + zRotation + xSpeed);
-		// double leftRearSpeed = ySpeed - zRotation + xSpeed;
-		// double rightRearSpeed = -(ySpeed + zRotation - xSpeed);
-		//
-		// leftFrontSpeed = capSpeed(leftFrontSpeed);
-		// rightFrontSpeed = capSpeed(rightFrontSpeed);
-		// leftRearSpeed = capSpeed(leftRearSpeed);
-		// rightRearSpeed = capSpeed(rightRearSpeed);
-		//
-		// Actuators.getLeftFrontMotor().set(ControlMode.PercentOutput, leftFrontSpeed);
-		// Actuators.getRightFrontMotor().set(ControlMode.PercentOutput,
-		// rightFrontSpeed);
-		// Actuators.getLeftRearMotor().set(ControlMode.PercentOutput, leftRearSpeed);
-		// Actuators.getRightRearMotor().set(ControlMode.PercentOutput, rightRearSpeed);
+	public static void mecDrive(double rightSpeed, double forwardSpeed, double zRotation) {
+		robotDrive.driveCartesian(sgnSquare(rightSpeed), sgnSquare(forwardSpeed), sgnSquare(zRotation));
+	}
+
+	public static void mecDrive(double rightSpeed, double forwardSpeed, double zRotation, double gyroAngle) {
+		robotDrive.driveCartesian(sgnSquare(rightSpeed), sgnSquare(forwardSpeed), sgnSquare(zRotation), gyroAngle);
 	}
 
 }
