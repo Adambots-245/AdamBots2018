2e2b40ac1a26fa334c73652dce13c9fd6b462a83
DESKTOP-DH7B3IV\Cobi <wkaza3748@rcs-k12.us>
2018-02-23 20:39:05 -0500

    Added Time remaining in Match to SmartDashboard

--- src/com/github/adambots/powerup2018/smartDash/SmartDash.java
+++ src/com/github/adambots/powerup2018/smartDash/SmartDash.java
@@ -1,31 +1,32 @@
 package com.github.adambots.powerup2018.smartDash;
-
+import edu.wpi.first.wpilibj.Timer;
 import org.usfirst.frc.team245.robot.Actuators;
 import org.usfirst.frc.team245.robot.Sensors;
 
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.Sendable;
 public class SmartDash {
-	
+
 	public static void init() {
 		CameraServer.getInstance().startAutomaticCapture();
 	}
-	
+
 	public static boolean isMotorRunning(SpeedController motor) {
 		double speed = motor.get();
 		return Math.abs(speed) > 0;
 	}
-	
+
 	public static double Voltage() {
 		PowerDistributionPanel pdp = Sensors.getPowerDistributionPanel();
 		double voltage = pdp.getVoltage();
 		return voltage;
 	}
 
-	
+
 	public static void Currents() {
 		PowerDistributionPanel pdp = Sensors.getPowerDistributionPanel();
 		int numChannels = 16;
@@ -33,8 +34,15 @@ public class SmartDash {
 			double current = pdp.getCurrent(i);
 			SmartDashboard.putNumber("Current " + i , current);
 		}
+	}	
+	public static double getMatchTime() {
+		DriverStation ds = DriverStation.getInstance();
+		double time = ds.getMatchTime();
+		return time;
 	}
-	
+
+
+
 	public static void updateDash() {
 		Currents();
 		SmartDashboard.putNumber("Voltage", Voltage());
@@ -46,5 +54,6 @@ public class SmartDash {
 		SmartDashboard.putBoolean("photoEyeStatis" , Sensors.getPhotoEyeValue());
 		SmartDashboard.putNumber("encoderVaule" , Actuators.getCarriageLiftMotorPosition());
 		SmartDashboard.putNumber("gyroAngle" , Sensors.getAngle());
-				}
+		SmartDashboard.putNumber("matchTime" , getMatchTime());
+	}
 }
