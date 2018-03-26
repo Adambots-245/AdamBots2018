aee903a6848bde0ff1b1126dd5636ac49c8c6943
245 Drive Station <jack.trueax@yahoo.com>
2018-03-26 18:59:04 -0400

    part of previous commit.

---
+++ src/com/github/adambots/powerup2018/auton/AutonRoutine.java
@@ -0,0 +1,13 @@
+package com.github.adambots.powerup2018.auton;
+
+public class AutonRoutine {
+
+	
+	public void initialize() {
+		
+	}
+	public void execute() {
+		
+	}
+	
+}
---
+++ src/com/github/adambots/powerup2018/autonModes/Scale.java
@@ -0,0 +1,68 @@
+package com.github.adambots.powerup2018.autonModes;
+
+import org.usfirst.frc.team245.robot.Actuators;
+
+import com.github.adambots.powerup2018.auton.AutonConstants;
+import com.github.adambots.powerup2018.auton.AutonRoutine;
+//import com.github.adambots.powerup2018.auton.Play;
+import com.github.adambots.powerup2018.auton.Time;
+import com.github.adambots.powerup2018.drive.Drive;
+import com.github.adambots.powerup2018.field.Field;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Scale extends AutonRoutine{
+
+	private boolean leftScaleValue;
+	private boolean rightScaleValue;
+	double time;
+	private static char position = Field.getScale(); 
+	
+		public Scale(){
+
+		}	
+
+		protected void init(){
+			time = Time.getTime();
+			
+			if(position != 'L'){
+				rightScaleValue = true;
+			}else{
+				leftScaleValue = true;	
+			}
+			if(leftScaleValue){
+//				Play.readRecording("/Documents/ghostMode.txt"); ///TODO: call actual pathway on roborio
+			}
+			else if(rightScaleValue){
+//				Play.readRecording("/Documents/ghostMode.txt");
+			}
+		}
+		public void execute() { //check the cross baseline values 
+			/*if(time < AutonConstants.GHOSTCODE_CROSS_TIME) {
+				Drive.autonDrive(0, AutonConstants.SWITCH_SPEED, 0);
+			}*/
+			//if(leftScaleValue){
+				//Play.playRecording();			
+
+			//}
+
+		}
+	
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	protected void end() {
+		Drive.autonDrive(0, 0, 0, 0);
+		Actuators.setCarriageLiftMotorSpeed(0);
+
+	}
+
+	
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+
+	}
+}
